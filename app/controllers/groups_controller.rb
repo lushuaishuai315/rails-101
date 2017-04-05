b146bdcfdd441263490a5b442854131c22ed1186
myOneTest <thunder_yhz@hotmail.com>
2017-04-05 19:12:02 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -9,6 +10,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
+
     @group = Group.find(params[:id])
 
     if current_user != @group.user
@@ -17,13 +19,14 @@ class GroupsController < ApplicationController
   end
 
   def new
+
     @group = Group.new
 
 end
 
 def create
   @group = Group.new(group_params)
-  @group.user = current_user。
+  @group.user = current_user
 
   if @group.save
     redirect_to groups_path
@@ -47,6 +50,7 @@ def update
   end
 
   def destroy
+
     @group = Group.find(params[:id])
 
     if current_user != @group.user
@@ -58,6 +62,14 @@ def update
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
